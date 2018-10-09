34c4aaa17cd2ab13e6b82309094ca09c8a1c801f
SchneiderMan <jschneid94@gmail.com>
2018-10-08 20:02:47 -0700

    Setup server, working on routes

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+var db = require("./models");
+
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+// ROUTES
+require("./routes/htmlRoutes")(app);
+require("./routes/apiRoutes")(app);
+
+app.listen(PORT, function() {
+    console.log(`Listening on port ${PORT}`);
+});
