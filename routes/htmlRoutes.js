0496c6ec07a7235f0649416dc441313e459d2c4a
SchneiderMan <jschneid94@gmail.com>
2018-10-09 22:40:18 -0700

    Added html route

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,7 +3,15 @@ var cheerio = require("cheerio");
 
 module.exports = function(app) {
     app.get("/", function(req, res) {
-        res.render("home");
+        db.Article.find({})
+          .then(function(dbArticle) {
+            // If we were able to successfully find Articles, send them back to the client
+            res.render("home", dbArticle);
+          })
+          .catch(function(err) {
+            // If an error occurred, send it to the client
+            res.json(err);
+          });
     });
 
     app.get("/saved", function(req, res) {
